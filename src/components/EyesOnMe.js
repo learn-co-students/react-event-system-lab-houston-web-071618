ecd257f3271ad32f0e67bcf72aeb3bf7377ce9fe
Christopher Arnold <chrisarnold2010@gmail.com>
2018-09-19 13:57:44 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
 // Code EyesOnMe Component Here
+
+import React, { Component } from 'react';
+class EyesOnMe extends Component {
+
+    getFocus = () => {
+        console.log('Good!');
+    }
+
+    getBlur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render() {
+        return (
+            <button 
+                onFocus={this.getFocus}
+                onBlur={this.getBlur}
+            >
+            </button>
+        );
+    }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,28 @@
 // Code Keypad Component Here
+
+import React, { Component } from 'react';
+
+class Keypad extends Component {
+
+
+    grabPassword = () => {
+        console.log('Entering password...');
+    }
+
+    render() {
+        return (
+            <input 
+                type="password"
+                // value={this.props.password}
+                onKeyUp={this.grabPassword}
+            />
+        );
+    }
+}
+
+export default Keypad;
+
+// In the components/Keypad.js file, create a Keypad React component.
+// In that component, render an input with the right type.
+// On that input, add an event handler that listens for the keyUp event.
+// When that event fires, use console.log to print out the text 'Entering password...'.
