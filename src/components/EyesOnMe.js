55f00e1ad9941e8d2b30c4650b2a4a617c8270bc
stephen <stephen@farver.io>
2018-09-18 23:31:23 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React, {Component} from 'react'
+
+class EyesOnMe extends Component {
+  goodText = () => {
+    console.log("Good!")
+  }
+
+  badText = () => {
+    console.log("Hey! Eyes on me!");
+  }
+
+  render () {
+    return (
+      <button onFocus={this.goodText} onBlur={this.badText}>Good</button>
+    )
+  }
+}
+
+export default EyesOnMe
